1c81f01bcce3caee4fe360cc6427d14d19508f16
Priyanshu Rai <priyanshurai439@gmail.com>
2025-09-04 14:07:38 +0530

    Resolve merge conflicts

--- src/test/java/com/example/interviewprep/InterviewprepApplicationTests.java
+++ src/test/java/com/example/interviewprep/InterviewprepApplicationTests.java
@@ -1,13 +1,26 @@
 package com.example.interviewprep;
 
+import com.example.interviewprep.repository.CompanyQuestionRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.ApplicationContext;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(properties = "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration")
 class InterviewprepApplicationTests {
+
+    @MockBean
+    private CompanyQuestionRepository repository;
+
+    @Autowired
+    private ApplicationContext context;
 
     @Test
     void contextLoads() {
+        assertThat(context).isNotNull();
     }
 
 }
